5f747d8f3f488fa7f8aec5841eaafcec49cbb0d2
SajjadHussain-UniBonn <sajjadravianuetian@gmail.com>
2024-08-07 00:04:05 +0200

    remove unrequired functions

--- app/main.cpp
+++ app/main.cpp
@@ -2,143 +2,35 @@
 #include "dataloader.hpp"
 #include "viewer.hpp"
 #include <open3d/Open3D.h>
-using namespace dataset;
 
 int main()
-{
-    const int choice = 4;
-    if(choice == 1)
+{   
+    // Get data path through CMake_variable
+    std::string data_root_dir = PROJECT_ROOT_DIR;
+    //Read laserscan data directory in a list
+    dataset::LaserScanDataset ls_dataset(data_root_dir);
+    std::vector<Eigen::Vector2d> target; 
+    std::vector<Eigen::Vector2d> source=ls_dataset[0];
+    std::vector<Eigen::Vector2d> transformed_PC;
+    Eigen::Matrix3d transformation_matrix;
+    const double grid_size = 0.1;
+    const double donsample_gridsize = 0.085;
+    const int repetition_score = 1;
+    int total_scans = ls_dataset.size();
+    std::cout<<"Progress of Registering Clouds"<<std::endl;
+    for(int i=1;i<total_scans;i++)
     {
-        const std::string filename = "/home/hussain/Work/CPP_Projects/Ex5/data/src.ply";
-        const std::string filename1 = "/home/hussain/Work/CPP_Projects/Ex5/data/target.ply";
-
-        open3d::geometry::PointCloud pcd;
-        open3d::geometry::PointCloud pcd1;
-
-        open3d::io::ReadPointCloud(filename, pcd);
-        open3d::io::ReadPointCloud(filename1, pcd1);
-
-        std::vector<Eigen::Vector3d> points;
-        std::vector<Eigen::Vector3d> points1;
-
-        points = pcd.points_;
-        points1 = pcd1.points_;
-
-        std::vector<Eigen::Vector2d> src;
-        std::vector<Eigen::Vector2d> target;
-        // convert 3D points to 2D points
-        std::transform(points.begin(), points.end(), std::back_inserter(src), [](const auto &p)
-                { return Eigen::Vector2d(p.x(), p.y()); });
-        std::transform(points1.begin(), points1.end(), std::back_inserter(target), [](const auto &p)
-                    { return Eigen::Vector2d(p.x(), p.y()); });
-
-        std::cout << "Read points from both pointclouds successfully!" << std::endl;
-        Eigen::Matrix3d transformation_matrix = ICP(src,target,3);
-        std::vector<Eigen::Vector2d> transformed_PC = ApplyTransformation(src,transformation_matrix);
-        viewCloud(transformed_PC);
+        target = ls_dataset[i];
+        LaserScanRegistration::ICP(source,target,grid_size);
+        source.insert(source.end(),target.begin(),target.end());
+        source = LaserScanRegistration::DownSample(source,donsample_gridsize,repetition_score);
+        target.clear();
+        std::cout<<"\r "<<i<<std::flush;
     }
-    if(choice==2)
-    {
-        // Get data path through CMake_variable
-        std::string data_root_dir = PROJECT_ROOT_DIR;
-        //Read laserscan data directory in a list
-        LaserScanDataset ls_dataset(data_root_dir);
-        std::vector<Eigen::Vector2d> target = ls_dataset[0];
-        std::vector<Eigen::Vector2d> source;
-        std::vector<Eigen::Vector2d> transformed_PC;
-        std::vector<Eigen::Vector2d> registered_PCs= target;
-        Eigen::Matrix3d transformation_matrix;
-        const double grid_size = 0.5;
-        int total_scans = 1500;
-        std::cout<<"Progress of Registering Clouds"<<std::endl;
-        for(int i=1;i<=total_scans;i++)
-        {
-            source = ls_dataset[i];
-            transformation_matrix = ICP(source,target,grid_size);
-            transformed_PC = ApplyTransformation(source,transformation_matrix);
-            registered_PCs.insert(registered_PCs.end(),transformed_PC.begin(),transformed_PC.end());
-            registered_PCs = DownSample(registered_PCs,0.1,1);
-            target.clear();
-            target = transformed_PC;
-            std::cout<<"\r "<<i<<std::flush;
-            source.clear();
-            transformed_PC.clear();
-        }
-        std::cout<<std::endl;
-        std::cout<<"Size of registered Point Cloud"<<std::endl;
-        std::cout<<registered_PCs.size()<<std::endl;
-        std::cout<<"Size of final target Point Cloud"<<std::endl;
-        std::cout<<target.size()<<std::endl;
-        viewCloud(registered_PCs);
-        viewCloud(target);
-        std::cout<<"Process Completed"<<std::endl;
-    }
-    if(choice==3)
-    {
-        // Get data path through CMake_variable
-        std::string data_root_dir = PROJECT_ROOT_DIR;
-        //Read laserscan data directory in a list
-        LaserScanDataset ls_dataset(data_root_dir);
-        std::vector<Eigen::Vector2d> target = ls_dataset[0];
-        std::vector<Eigen::Vector2d> source;
-        std::vector<Eigen::Vector2d> transformed_PC;
-        std::vector<Eigen::Vector2d> registered_PCs= target;
-        Eigen::Matrix3d transformation_matrix;
-        const double grid_size = 0.2;
-        int total_scans = ls_dataset.size();
-        std::cout<<"Progress of Registering Clouds"<<std::endl;
-        for(int i=1;i<=total_scans;i++)
-        {
-            source = ls_dataset[i];
-            transformation_matrix = ICP(source,target,grid_size);
-            transformed_PC = ApplyTransformation(source,transformation_matrix);
-            registered_PCs.insert(registered_PCs.end(),transformed_PC.begin(),transformed_PC.end());
-            registered_PCs = DownSample(registered_PCs,0.1,1);
-            target.clear();
-            target = registered_PCs;
-            std::cout<<"\r "<<i<<std::flush;
-            source.clear();
-            transformed_PC.clear();
-        }
-        std::cout<<std::endl;
-        std::cout<<"Size of registered Point Cloud"<<std::endl;
-        std::cout<<registered_PCs.size()<<std::endl;
-        std::cout<<"Size of final target Point Cloud"<<std::endl;
-        std::cout<<target.size()<<std::endl;
-        viewCloud(registered_PCs);
-        viewCloud(target);
-        std::cout<<"Process Completed"<<std::endl;
-    }
-    if(choice==4)
-    {
-        // Get data path through CMake_variable
-        std::string data_root_dir = PROJECT_ROOT_DIR;
-        //Read laserscan data directory in a list
-        LaserScanDataset ls_dataset(data_root_dir);
-        std::vector<Eigen::Vector2d> target; 
-        std::vector<Eigen::Vector2d> source=ls_dataset[0];
-        std::vector<Eigen::Vector2d> transformed_PC;
-        // std::vector<Eigen::Vector2d> registered_PCs;
-        Eigen::Matrix3d transformation_matrix;
-        const double grid_size = 0.1;
-        int total_scans = ls_dataset.size();
-        // int total_scans = 3000;
-        std::cout<<"Progress of Registering Clouds"<<std::endl;
-        for(int i=1;i<total_scans;i++)
-        {
-            target = ls_dataset[i];
-            transformation_matrix = ICP(source,target,grid_size);
-            source = ApplyTransformation(source,transformation_matrix);
-            source.insert(source.end(),target.begin(),target.end());
-            source = DownSample(source,0.085,1);
-            target.clear();
-            std::cout<<"\r "<<i<<std::flush;
-        }
-        std::cout<<std::endl;
-        std::cout<<"Size of registered Point Cloud"<<std::endl;
-        std::cout<<source.size()<<std::endl;
-        viewCloud(source);
-        std::cout<<"Process Completed"<<std::endl;
-    }
+    std::cout<<std::endl;
+    std::cout<<"Size of registered Point Cloud"<<std::endl;
+    std::cout<<source.size()<<std::endl;
+    LaserScanRegistration::viewCloud(source);
+    std::cout<<"Process Completed"<<std::endl;
     return 0;
 }
--- viewer/viewer.cpp
+++ viewer/viewer.cpp
@@ -6,224 +6,188 @@
 #include <utility>
 #include <vector>
 
-void viewCloud(const std::vector<Eigen::Vector2d> &pcd) 
+namespace LaserScanRegistration
 {
-  std::vector<Eigen::Vector3d> pts(pcd.size());
-  std::transform(pcd.cbegin(), pcd.cend(), pts.begin(), [](const auto &p) {
-    return Eigen::Vector3d(p.x(), p.y(), 0.0);
-  });
-  open3d::geometry::PointCloud pointcloud{pts};
-  pointcloud.PaintUniformColor(Eigen::Vector3d(1.0, 0.0, 0.0));
-  open3d::visualization::DrawGeometries(
-      {std::make_shared<open3d::geometry::PointCloud>(pointcloud)});
-}
-Eigen::Vector2d ComputeMean(const std::vector<Eigen::Vector2d> &pcd)
-{
-  Eigen::Vector2d zero = Eigen::Vector2d::Zero();
-  Eigen::Vector2d sum = std::accumulate(pcd.begin(), pcd.end(), zero);
-  Eigen::Vector2d mean = sum / pcd.size();
-  return mean;
-}
-Eigen::Matrix2d ComputeCovariance(const std::vector<Eigen::Vector2d> &source, 
-const std::vector<Eigen::Vector2d> &target,const Eigen::Vector2d source_mean, 
-const Eigen::Vector2d target_mean)
-{
-  Eigen::Matrix2d covariance_matrix = Eigen::Matrix2d::Zero();
-  for(size_t i=0;i< source.size(); i++)
+  void viewCloud(const std::vector<Eigen::Vector2d> &pcd) 
   {
-    Eigen::Vector2d source_deviation = source[i] - source_mean;
-    Eigen::Vector2d target_deviation = target[i] - target_mean;
-    covariance_matrix += source_deviation * target_deviation.transpose();
+    std::vector<Eigen::Vector3d> pts(pcd.size());
+    std::transform(pcd.cbegin(), pcd.cend(), pts.begin(), [](const auto &p) {
+      return Eigen::Vector3d(p.x(), p.y(), 0.0);
+    });
+    open3d::geometry::PointCloud pointcloud{pts};
+    pointcloud.PaintUniformColor(Eigen::Vector3d(1.0, 0.0, 0.0));
+    open3d::visualization::DrawGeometries(
+        {std::make_shared<open3d::geometry::PointCloud>(pointcloud)});
   }
-  return covariance_matrix;
-}
-Eigen::Matrix3d ComputeTransformation(const std::vector<Eigen::Vector2d> &source, 
-const std::vector<Eigen::Vector2d> &target)
-{
-  Eigen::Matrix3d transformation_matrix = Eigen::Matrix3d::Zero();
-  Eigen::Vector2d target_mean = ComputeMean(target);
-  Eigen::Vector2d source_mean = ComputeMean(source);
-  Eigen::Matrix2d covariance_matrix = ComputeCovariance(source,target,source_mean,target_mean);
-  Eigen::JacobiSVD<Eigen::Matrix2d> svd(covariance_matrix,Eigen::ComputeFullU | Eigen::ComputeFullV);
-  Eigen::Matrix2d U = svd.matrixU();
-  Eigen::Matrix2d V = svd.matrixV();
-  Eigen::Matrix2d rotation_matrix = V*(U.transpose());
-  Eigen::Vector2d translation_vector = target_mean - (rotation_matrix*source_mean);
-  transformation_matrix.block<2,2>(0,0) = rotation_matrix;
-  transformation_matrix.block<2,1>(0,2) = translation_vector;
-  transformation_matrix(2,2) = 1;
-  return transformation_matrix;
-}
-std::vector<Eigen::Vector2d> ApplyTransformation(const std::vector<Eigen::Vector2d> &pcd,
-const Eigen::Matrix3d &transformation_matrix)
-{
-  std::vector<Eigen::Vector2d> tranformed_pc;
-  //Get Transformation
-  Eigen::Matrix2d rotation_matrix = transformation_matrix.block<2,2>(0,0);
-  Eigen::Vector2d translation_vector = transformation_matrix.block<2,1>(0,2);
-  //Transform point cloud point by point
-  for(size_t i=0; i< pcd.size();i++)
+  Eigen::Vector2d ComputeMean(const std::vector<Eigen::Vector2d> &pcd)
   {
-    tranformed_pc.push_back(rotation_matrix*pcd[i] + translation_vector);
+    Eigen::Vector2d zero = Eigen::Vector2d::Zero();
+    Eigen::Vector2d sum = std::accumulate(pcd.begin(), pcd.end(), zero);
+    Eigen::Vector2d mean = sum / pcd.size();
+    return mean;
   }
-  //Release the extra allocated  memory
-  tranformed_pc.shrink_to_fit();
-  return tranformed_pc;
-}
-double ComputeError(const std::vector<Eigen::Vector2d> &target_pc, 
-const std::vector<Eigen::Vector2d> &transformed_pc)
-{
-  double error = 0;
-  for(size_t i=0; i<target_pc.size();i++)
+  Eigen::Matrix2d ComputeCovariance(const std::vector<Eigen::Vector2d> &source, 
+  const std::vector<Eigen::Vector2d> &target,const Eigen::Vector2d &source_mean, 
+  const Eigen::Vector2d &target_mean)
   {
-    error += (target_pc[i] - transformed_pc[i]).squaredNorm();
-  }
-  return error;
-}
-std::unordered_map<GridIndex,std::vector<Eigen::Vector2d>> CreateGridMap(const std::vector<Eigen::Vector2d> &pcd, 
-double grid_size)
-{
-  std::unordered_map<GridIndex, std::vector<Eigen::Vector2d>> grid;
-  for (const auto &point : pcd) 
-  {
-    GridIndex index {static_cast<int>(point[0] / grid_size),
-    static_cast<int>(point[1] / grid_size)};
-    grid[index].push_back(point);
-  }
-  return grid;
-}
-std::tuple<std::vector<Eigen::Vector2d>, std::vector<Eigen::Vector2d>> FindNearestNeighbors(const std::unordered_map<GridIndex,std::vector<Eigen::Vector2d>> &grid, 
-const std::vector<Eigen::Vector2d> &source_pc, double grid_size)
-{
-  std::vector<Eigen::Vector2d> source_correspondences;
-  std::vector<Eigen::Vector2d> target_correspondences;
-  double minimum_distance = INFINITY;
-  double distance = INFINITY;
-  Eigen::Vector2d nearest_neighbor = Eigen::Vector2d::Zero();
-  bool neighbor_found = false;
-  for(const Eigen::Vector2d &query_point:source_pc)
-  {
-    neighbor_found = false;
-    minimum_distance = INFINITY;
-    GridIndex query_index {static_cast<int>(query_point[0] / grid_size), 
-    static_cast<int>(query_point[1] / grid_size)};
-    // Iterate through the current cell and adjacent cells
-    for (int dx = -1; dx <= 1; ++dx) 
+    Eigen::Matrix2d covariance_matrix = Eigen::Matrix2d::Zero();
+    for(size_t i=0;i< source.size(); i++)
     {
-      for (int dy = -1; dy <= 1; ++dy) 
+      covariance_matrix += (source[i] - source_mean) * (target[i] - target_mean).transpose();
+    }
+    return covariance_matrix;
+  }
+  Eigen::Matrix3d ComputeTransformation(const std::vector<Eigen::Vector2d> &source, 
+  const std::vector<Eigen::Vector2d> &target)
+  {
+    Eigen::Matrix3d transformation_matrix = Eigen::Matrix3d::Zero();
+    Eigen::Vector2d target_mean = ComputeMean(target);
+    Eigen::Vector2d source_mean = ComputeMean(source);
+    Eigen::Matrix2d covariance_matrix = ComputeCovariance(source,target,source_mean,target_mean);
+    Eigen::JacobiSVD<Eigen::Matrix2d> svd(covariance_matrix,Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::Matrix2d U = svd.matrixU();
+    Eigen::Matrix2d V = svd.matrixV();
+    Eigen::Matrix2d rotation_matrix = V*(U.transpose());
+    Eigen::Vector2d translation_vector = target_mean - (rotation_matrix*source_mean);
+    transformation_matrix.block<2,2>(0,0) = rotation_matrix;
+    transformation_matrix.block<2,1>(0,2) = translation_vector;
+    transformation_matrix(2,2) = 1;
+    return transformation_matrix;
+  }
+  std::vector<Eigen::Vector2d> ApplyTransformation(std::vector<Eigen::Vector2d> &pcd,
+  const Eigen::Matrix3d &transformation_matrix)
+  {
+    Eigen::Matrix2d rotation_matrix = transformation_matrix.block<2,2>(0,0);
+    Eigen::Vector2d translation_vector = transformation_matrix.block<2,1>(0,2);
+    std::transform(pcd.cbegin(), pcd.cend(), pcd.begin(), 
+                    [&rotation_matrix, &translation_vector](const auto &point)
+                    { return rotation_matrix * point + translation_vector;});
+    return pcd;
+  }
+  double ComputeError(const std::vector<Eigen::Vector2d> &target_pc, 
+  const std::vector<Eigen::Vector2d> &transformed_pc)
+  {
+    double error = 0;
+    for(size_t i=0; i<target_pc.size();i++)
+    {
+      error += (target_pc[i] - transformed_pc[i]).squaredNorm();
+    }
+    return error;
+  }
+  std::unordered_map<GridIndex,std::vector<Eigen::Vector2d>> CreateGridMap(const std::vector<Eigen::Vector2d> &pcd, 
+  double grid_size)
+  {
+    std::unordered_map<GridIndex, std::vector<Eigen::Vector2d>> grid;
+    for (const auto &point : pcd) 
+    {
+      GridIndex index {static_cast<int>(point[0] / grid_size),
+      static_cast<int>(point[1] / grid_size)};
+      grid[index].push_back(point);
+    }
+    return grid;
+  }
+  std::tuple<std::vector<Eigen::Vector2d>, std::vector<Eigen::Vector2d>> FindNearestNeighbors(const std::unordered_map<GridIndex,std::vector<Eigen::Vector2d>> &grid, 
+  const std::vector<Eigen::Vector2d> &source_pc, double grid_size)
+  {
+    std::vector<Eigen::Vector2d> source_correspondences;
+    std::vector<Eigen::Vector2d> target_correspondences;
+    Eigen::Vector2d nearest_neighbor = Eigen::Vector2d::Zero();
+    double minimum_distance = INFINITY;
+    double distance = INFINITY;
+    bool neighbor_found = false;
+    for(const Eigen::Vector2d &query_point:source_pc)
+    {
+      neighbor_found = false;
+      minimum_distance = INFINITY;
+      GridIndex query_index {static_cast<int>(query_point[0] / grid_size), 
+      static_cast<int>(query_point[1] / grid_size)};
+      // Iterate through the current cell and adjacent cells
+      for (int dx = -1; dx <= 1; ++dx) 
       {
-        GridIndex neighbor_index {query_index.x + dx, query_index.y + dy};
-        // Check if the neighbor cell exists in the grid map
-        if (grid.find(neighbor_index) != grid.end()) 
+        for (int dy = -1; dy <= 1; ++dy) 
         {
-          neighbor_found = true;
-          std::vector<Eigen::Vector2d> list = grid.at(neighbor_index);
-          for (const auto &point : list) 
+          GridIndex neighbor_index {query_index.x + dx, query_index.y + dy};
+          // Check if the neighbor cell exists in the grid map
+          if (grid.find(neighbor_index) != grid.end()) 
           {
-            distance = (point - query_point).norm();
-            if (distance < minimum_distance) 
+            neighbor_found = true;
+            //Get all points at that key
+            std::vector<Eigen::Vector2d> list = grid.at(neighbor_index);
+            for (const auto &point : list) 
             {
-              minimum_distance = distance;
-              nearest_neighbor = point;
+              distance = (point - query_point).norm();
+              if (distance < minimum_distance) 
+              {
+                minimum_distance = distance;
+                nearest_neighbor = point;
+              }
             }
           }
         }
       }
+      if(neighbor_found)
+      {
+        source_correspondences.emplace_back(query_point);
+        target_correspondences.emplace_back(nearest_neighbor);
+      }
     }
-    if(neighbor_found)
-    {
-      source_correspondences.emplace_back(query_point);
-      target_correspondences.emplace_back(nearest_neighbor);
-    }
+    //Release extra memory
+    source_correspondences.shrink_to_fit();
+    target_correspondences.shrink_to_fit();
+    return  std::make_tuple(source_correspondences,target_correspondences);
   }
-  return  std::make_tuple(source_correspondences,target_correspondences);
-}
-Eigen::Matrix3d ICP(const std::vector<Eigen::Vector2d> &source_, 
-const std::vector<Eigen::Vector2d> &target,const double &grid_size)
-{
-  std::vector<Eigen::Vector2d> source = source_;
-  int maximum_iteration = 13;
-  int iteration_counter = 0;
-  double old_error = INFINITY;
-  double error = INFINITY;
-  Eigen::Matrix3d final_transformation_matrix = Eigen::Matrix3d::Identity();
-  Eigen::Matrix3d current_tranformation_matrix = Eigen::Matrix3d::Identity();
-  std::vector<Eigen::Vector2d> source_correspondences;
-  std::vector<Eigen::Vector2d> target_correspondences;
-  std::unordered_map<GridIndex,std::vector<Eigen::Vector2d>> grid_map = CreateGridMap(target,grid_size);
-  while(true)
+  void ICP(std::vector<Eigen::Vector2d> &source, 
+  const std::vector<Eigen::Vector2d> &target,const double &grid_size)
   {
-    iteration_counter++;
-    std::tuple<std::vector<Eigen::Vector2d>, std::vector<Eigen::Vector2d>> nearest_neighbors = 
-    FindNearestNeighbors(grid_map,source,grid_size);
-    source_correspondences = std::get<0>(nearest_neighbors);
-    target_correspondences = std::get<1>(nearest_neighbors);
-    // viewCloud(source_correspondences);
-    // viewCloud(target_correspondences);
-    // std::cout<<source_correspondences.size()<<std::endl;
-    // std::cout<<target_correspondences.size()<<std::endl;
-    current_tranformation_matrix = ComputeTransformation(source_correspondences,target_correspondences);
-    final_transformation_matrix = current_tranformation_matrix*final_transformation_matrix;
-    source= ApplyTransformation(source,current_tranformation_matrix);
-    error = ComputeError(target,source);
-    if(error==old_error || iteration_counter==maximum_iteration)
+    int maximum_iteration = 13;
+    int iteration_counter = 0;
+    double old_error = INFINITY;
+    double error = INFINITY;
+    Eigen::Matrix3d current_tranformation_matrix = Eigen::Matrix3d::Identity();
+    std::vector<Eigen::Vector2d> source_correspondences;
+    std::vector<Eigen::Vector2d> target_correspondences;
+    std::unordered_map<GridIndex,std::vector<Eigen::Vector2d>> grid_map = CreateGridMap(target,grid_size);
+    while(true)
     {
-      return final_transformation_matrix;
-      
+      iteration_counter++;
+      std::tuple<std::vector<Eigen::Vector2d>, std::vector<Eigen::Vector2d>> nearest_neighbors = 
+      FindNearestNeighbors(grid_map,source,grid_size);
+      source_correspondences = std::get<0>(nearest_neighbors);
+      target_correspondences = std::get<1>(nearest_neighbors);
+      current_tranformation_matrix = ComputeTransformation(source_correspondences,target_correspondences);
+      source= ApplyTransformation(source,current_tranformation_matrix);
+      error = ComputeError(target,source);
+      if(error==old_error || iteration_counter==maximum_iteration)
+      {
+        break; 
+      }
+      old_error = error;
+      source_correspondences.clear();
+      target_correspondences.clear();
     }
-    old_error = error;
-    // std::cout<<error<<std::endl;
-    source_correspondences.clear();
-    target_correspondences.clear();
   }
-}
-std::vector<Eigen::Vector2d> ConcatenatePointClouds(const std::vector<Eigen::Vector2d> &first, 
-const std::vector<Eigen::Vector2d> &second)
-{
-  std::vector<Eigen::Vector2d> result = first;
-  result.insert(result.end(),second.begin(),second.end());
-  return result;
-}
-std::vector<Eigen::Vector2d> RegisterPointClouds(const std::vector<Eigen::Vector2d> &source,
-const std::vector<Eigen::Vector2d> &target,const double &grid_size)
-{
-
-  std::vector<Eigen::Vector2d> transformed_PC;
-  std::vector<Eigen::Vector2d> registered_PCs;
-  Eigen::Matrix3d transformation_matrix = ICP(source,target,grid_size);
-  transformed_PC = ApplyTransformation(source,transformation_matrix);
-  registered_PCs = ConcatenatePointClouds(target,transformed_PC);
-  return registered_PCs;
-}
-std::vector<Eigen::Vector2d> RegisterPointClouds_KnownCorrespondences(const std::vector<Eigen::Vector2d> &source,
-const std::vector<Eigen::Vector2d> &target)
-{
-  std::vector<Eigen::Vector2d> transformed_PC;
-  std::vector<Eigen::Vector2d> registered_PCs;
-  Eigen::Matrix3d transformation_matrix = ComputeTransformation(source,target);
-  transformed_PC = ApplyTransformation(source,transformation_matrix);
-  registered_PCs = ConcatenatePointClouds(target,transformed_PC);
-  return registered_PCs;
-}
-std::vector<Eigen::Vector2d> DownSample(const std::vector<Eigen::Vector2d> &pcd, 
-const double &grid_size, const int &n_points)
-{
-  std::vector<Eigen::Vector2d> sampled_points;
-  std::unordered_map<GridIndex,int> grid2D;
-  for(const auto &query_point:pcd)
+  std::vector<Eigen::Vector2d> DownSample(const std::vector<Eigen::Vector2d> &pcd, 
+  const double &grid_size, const int &n_points)
   {
-    GridIndex query_index {static_cast<int>(query_point[0] / grid_size), 
-    static_cast<int>(query_point[1] / grid_size)};
-    if(grid2D.find(query_index)==grid2D.end())
+    std::vector<Eigen::Vector2d> sampled_points;
+    std::unordered_map<GridIndex,int> grid2D;
+    for(const auto &query_point:pcd)
     {
-      grid2D[query_index]=1;
-      sampled_points.emplace_back(query_point);
+      GridIndex query_index {static_cast<int>(query_point[0] / grid_size), 
+      static_cast<int>(query_point[1] / grid_size)};
+      if(grid2D.find(query_index)==grid2D.end())
+      {
+        grid2D[query_index]=1;
+        sampled_points.emplace_back(query_point);
+      }
+      else if(grid2D[query_index]< n_points)
+      {
+        grid2D[query_index]++;
+        sampled_points.emplace_back(query_point);
+      }
     }
-    else if(grid2D[query_index]< n_points)
-    {
-      grid2D[query_index]++;
-      sampled_points.emplace_back(query_point);
-    }
+    //Release extra memory
+    sampled_points.shrink_to_fit();
+    return sampled_points;
   }
-  return sampled_points;
-}
+}//end of namespace
